c9d1cae4fa946418543d3612147e679fda70ee6d
jsnmc <jsnmcdev@gmail.com>
2017-01-18 23:18:58 -0500

    Initial commit

---
+++ src/CoreCopy/CopyItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCopy
+{
+    public class CopyItem
+    {
+        public string source { get; set; }
+        public string dest { get; set; }
+        public List<string> include { get; set; }
+        public List<string> exclude { get; set; }
+        public bool overwrite { get; set; }
+    }
+
+    //    {
+    //  "source": null,
+    //  "dest": null,
+    //  "include": null,
+    //  "exclude": null,
+    //  "overwrite": false
+    //}
+
+}
--- src/CoreCopy/Program.cs
+++ src/CoreCopy/Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace CoreCopy
 {
@@ -9,6 +11,8 @@ namespace CoreCopy
     {
         public static void Main(string[] args)
         {
+            // read file into a string and deserialize JSON to a type
+            var x = JsonConvert.DeserializeObject<List<CopyItem>>(File.ReadAllText(@"test.json"));
         }
     }
 }
